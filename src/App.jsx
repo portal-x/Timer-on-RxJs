3fc3d8acdb7840a83ae13ca774a80d48d269976e
Yurii Tkachuk <papirus.borzna@gmail.com>
2021-07-06 17:00:43 +0300

    inject Subjects

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
 import './App.css';
-import React, { useState } from 'react';
-import { interval, Observable, Subject, NEVER, BehaviorSubject } from 'rxjs';
+import React, { useEffect, useState } from 'react';
+import { interval, Observable, Subject, NEVER, BehaviorSubject, observable } from 'rxjs';
 import { takeWhile, takeUntil, switchMap, repeatWhen, startWith, map } from 'rxjs/operators';
 import formatsTime from './secondToForm';
+
+const action$ = new Subject();
+const reset$ = new Subject();
+const wait$ = new Subject();
+const observable$ = interval(1000).pipe(
+  map(formatsTime),
+  takeUntil(wait$),
+);
 
 function App() {
   console.log('app is render');
   const [time, setTime] = useState(0);
   const [run, setRun] = useState(false);
 
-  const stopPlay$ = new Subject();
-  // stopPlay$.subscribe(v => console.log('sub:', v));
-  const observable$ = interval(1000).pipe(
-    takeUntil(stopPlay$),
-    map(formatsTime),
-    );
+  useEffect(() => {
+    console.log('mount');
+    const sub = observable$.subscribe(setTime);
+    return () => {
+      console.log('unmount');
+      return sub.unsubscribe()
+    };
+  }, []);
+
+  // const stopPlay$ = new Subject();
+  // const observable$ = interval(1000).pipe(
+  //   takeUntil(stopPlay$),
+  //   map(formatsTime),
+  //   );
 
   const handleStart = () => {
     setRun((prew) => !prew);
-      observable$.subscribe((v) => setTime(v));
-      // if (run) {
-      //   console.log('unsubscribe');
-      //   stopPlay$.next();
-      // }
+    action$.next('start');
+      // const sub = observable$.subscribe(setTime);
   };
-
+  
   const handleWait = () => {
-    stopPlay$.next(true);
-    console.log('unsubscribe');
+    wait$.next('wait');
   }
 
   return (
@@ -37,7 +49,7 @@ function App() {
       </div>
       <button className="start" onClick={handleStart}>Start / Stop</button>
       <button className="wait" onClick={handleWait}>Wait</button>
-      <button className="reset">Reset</button>
+      <button className="reset" onClick={() => reset$.next('reset')}>Reset</button>
     </div>
   );
 }
